e36137f0389e8b6d50c3e05b20765464fee5d389
Abeinevincent <abeinevincent@gmail.com>
2023-08-19 19:03:20 +0300

    Added album and toptracks

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-const PF = import.meta.env.VITE_REACT_APP_API_URL;
+// const PF = import.meta.env.VITE_REACT_APP_API_URL;
+const PF = "https://musicstreamingapp-a2500a5331b4.herokuapp.com/api/v1/";
 export const myAPIClient = axios.create({
   baseURL: `${PF}`,
 });
